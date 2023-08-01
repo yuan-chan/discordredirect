57a90a0c61a74e2b0e78a05296d5b736e288b979
yuan-chan <59510695+yuan-chan@users.noreply.github.com>
2023-08-02 00:53:36 +0900

    Blood-type: A

--- popup.js
+++ popup.js
@@ -1,21 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var saveButton = document.getElementById('saveButton');
-  var redirectOption = document.getElementById('redirectOption');
+document.addEventListener("DOMContentLoaded", function () {
+  var saveButton = document.getElementById("saveButton");
+  var redirectOption = document.getElementById("redirectOption");
 
-  chrome.storage.local.get('redirectOption', function(data) {
+  chrome.storage.local.get("redirectOption", function (data) {
     redirectOption.value = data.redirectOption;
   });
 
-  saveButton.addEventListener('click', function() {
-    chrome.storage.local.set({ redirectOption: redirectOption.value }, function() {
-      console.log('Redirect option saved: ' + redirectOption.value);
-    });
-    if (redirectOption.value === 'canary') {
-      chrome.storage.local.set({ redirectUrl: 'https://canary.discord.com/' });
-    } else if (redirectOption.value === 'ptb') {
-      chrome.storage.local.set({ redirectUrl: 'https://ptb.discord.com/' });
+  saveButton.addEventListener("click", function () {
+    chrome.storage.local.set(
+      { redirectOption: redirectOption.value },
+      function () {
+        console.log("Redirect option saved: " + redirectOption.value);
+      }
+    );
+    if (redirectOption.value === "canary") {
+      chrome.storage.local.set({ redirectUrl: "https://canary.discord.com/" });
+    } else if (redirectOption.value === "ptb") {
+      chrome.storage.local.set({ redirectUrl: "https://ptb.discord.com/" });
     } else {
-      chrome.storage.local.set({ redirectUrl: 'https://discord.com/' });
+      chrome.storage.local.set({ redirectUrl: "https://discord.com/" });
     }
   });
 });
